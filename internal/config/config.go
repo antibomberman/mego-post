1ec21847bbc1caae1b1db2a482fb5d6f7fc15322
agabek <agabek309@gmail.com>
2024-07-30 10:09:40 +0500

    init

---
+++ internal/config/config.go
@@ -0,0 +1,18 @@
+package config
+
+import "log"
+
+type Config struct {
+	DatabaseURL string `yaml:"database_url" required:"true"`
+	JWTSecret   string `yaml:"jwt_secret" required:"true"`
+	ServerPort  string `yaml:"server_port" required:"true"`
+}
+
+func Load() *Config {
+	path := "./config/local.yaml"
+	var cfg Config
+	if err := cleanenv.ReadConfig(path, &cfg); err != nil {
+		log.Fatalf("failed to read config: %v", err)
+	}
+	return &cfg
+}
