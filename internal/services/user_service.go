b8541f4b7e7c9477faaf9226c2d688d742fd4e70
agabek <agabek309@gmail.com>
2024-07-31 15:21:38 +0500

    comment

--- internal/services/user_service.go
+++ internal/services/user_service.go
@@ -1,29 +1,21 @@
 package services
 
-import (
-	"context"
-	pb "github.com/megotours/proto/auth"
-	"google.golang.org/grpc"
-	"log"
-	"time"
-)
-
 func Connect() {
-	conn, err := grpc.NewClient("localhost:50051")
-	if err != nil {
-		log.Fatalf("Не удалось подключиться: %v", err)
-	}
-	defer conn.Close()
+	//conn, err := grpc.NewClient("localhost:50051")
+	//if err != nil {
+	//	log.Fatalf("Не удалось подключиться: %v", err)
+	//}
+	//defer conn.Close()
+	//
+	//client := pb.NewUserClient(conn)
+	//
+	//ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	//defer cancel()
 
-	client := pb.NewUsersClient(conn)
-
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
-
-	response, err := client.Get(ctx, &pb.GetByIdRequest{Id: "1"})
-	if err != nil {
-		log.Fatalf("Не удалось получить данные: %v", err)
-	}
-
-	log.Printf("Ответ: %s", response.GetUser())
+	//response, err := client.GetById(ctx, &pb.GetByIdRequest{Id: "1"})
+	//if err != nil {
+	//	log.Fatalf("Не удалось получить данные: %v", err)
+	//}
+	//
+	//log.Printf("Ответ: %s", response.GetUser())
 }
